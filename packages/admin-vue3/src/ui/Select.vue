4c9349c0f42fc40b5d563b1decdfae72d9d8e97c
cbl <lennart.carbe@gmail.com>
2021-06-23 12:37:27 +0200

    wip

--- packages/admin-vue3/src/ui/Select.vue
+++ packages/admin-vue3/src/ui/Select.vue
@@ -1,12 +1,143 @@
 <template>
-    <base-select v-bind="$attrs" />
+    <Listbox as="div" v-bind="$attrs">
+        <div class="mt-1 relative">
+            <ListboxButton
+                class="
+                    relative
+                    w-full
+                    bg-white
+                    border border-gray-300
+                    rounded-md
+                    shadow-sm
+                    pl-3
+                    pr-10
+                    py-2
+                    text-left
+                    cursor-default
+                    focus:outline-none
+                    focus:ring-1
+                    focus:ring-indigo-500
+                    focus:border-indigo-500
+                    sm:text-sm
+                "
+            >
+                <span class="flex items-center">
+                    <span class="ml-3 block truncate">{{ modelValue }}</span>
+                </span>
+                <span
+                    class="
+                        ml-3
+                        absolute
+                        inset-y-0
+                        right-0
+                        flex
+                        items-center
+                        pr-2
+                        pointer-events-none
+                    "
+                >
+                    <span class="h-5 w-5 text-gray-400" aria-hidden="true">
+                        i
+                    </span>
+                </span>
+            </ListboxButton>
+
+            <transition
+                leave-active-class="transition ease-in duration-100"
+                leave-from-class="opacity-100"
+                leave-to-class="opacity-0"
+            >
+                <ListboxOptions
+                    class="
+                        absolute
+                        z-10
+                        mt-1
+                        w-full
+                        bg-white
+                        shadow-lg
+                        max-h-56
+                        rounded-md
+                        py-1
+                        text-base
+                        ring-1 ring-black ring-opacity-5
+                        overflow-auto
+                        focus:outline-none
+                        sm:text-sm
+                    "
+                >
+                    <ListboxOption
+                        as="template"
+                        v-for="(label, value) in options"
+                        :key="value"
+                        :value="value"
+                        v-slot="{ active, selected }"
+                    >
+                        <li
+                            :class="[
+                                active
+                                    ? 'text-white bg-indigo-600'
+                                    : 'text-gray-900',
+                                'cursor-default select-none relative py-2 pl-3 pr-9',
+                            ]"
+                        >
+                            <div class="flex items-center">
+                                <span
+                                    :class="[
+                                        selected
+                                            ? 'font-semibold'
+                                            : 'font-normal',
+                                        'ml-3 block truncate',
+                                    ]"
+                                >
+                                    {{ label }}
+                                </span>
+                            </div>
+
+                            <span
+                                v-if="selected"
+                                :class="[
+                                    active ? 'text-white' : 'text-indigo-600',
+                                    'absolute inset-y-0 right-0 flex items-center pr-4',
+                                ]"
+                            >
+                                <span class="h-5 w-5" aria-hidden="true">
+                                    check
+                                </span>
+                            </span>
+                        </li>
+                    </ListboxOption>
+                </ListboxOptions>
+            </transition>
+        </div>
+    </Listbox>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { Select as BaseSelect } from '@macramejs/macrame-vue3';
+import {
+    Listbox,
+    ListboxButton,
+    ListboxLabel,
+    ListboxOption,
+    ListboxOptions,
+} from '@headlessui/vue';
 
 export default defineComponent({
-    components: { BaseSelect },
+    components: {
+        Listbox,
+        ListboxButton,
+        ListboxLabel,
+        ListboxOption,
+        ListboxOptions,
+    },
+    props: {
+        options: {
+            type: Object,
+            required: true,
+        },
+        modelValue: {
+            required: true,
+        },
+    },
 });
 </script>
