b5992cc73dcbd3039e165554a586184736b2791e
cbl <lennart.carbe@gmail.com>
2021-06-23 10:44:26 +0200

    wip

--- packages/admin-vue3/src/ui/Badge.vue
+++ packages/admin-vue3/src/ui/Badge.vue
@@ -3,20 +3,11 @@
         :is="tag"
         class="inline-flex justify-center items-center px-2 rounded-xs text-sm"
         :class="{
-            'bg-blue text-white ':
-                variant == 'blue' && !outline && !text && !disabled,
-
-            'bg-gray-700  text-white':
-                variant == 'gray' && !outline && !text && !disabled,
-
-            'bg-green  text-green-700':
-                variant == 'green' && !outline && !text && !disabled,
-
-            'bg-red  text-red-700':
-                variant == 'red' && !outline && !text && !disabled,
-
-            'bg-yellow  text-yellow-700':
-                variant == 'yellow' && !outline && !text && !disabled,
+            'bg-blue text-white ': comp_variant == 'blue',
+            'bg-gray-700  text-white': comp_variant == 'gray',
+            'bg-green  text-green-700': comp_variant == 'green',
+            'bg-red  text-red-700': comp_variant == 'red',
+            'bg-yellow  text-yellow-700': comp_variant == 'yellow',
         }"
     >
         <slot />
@@ -32,11 +23,55 @@ export default defineComponent({
             default: 'blue',
             type: String,
         },
+        blue: {
+            type: Boolean,
+            default: false,
+        },
+        gray: {
+            type: Boolean,
+            default: false,
+        },
+        green: {
+            type: Boolean,
+            default: false,
+        },
+        yellow: {
+            type: Boolean,
+            default: false,
+        },
+        red: {
+            type: Boolean,
+            default: false,
+        },
     },
     setup(props, { attrs }) {
         const tag = 'href' in attrs ? 'a' : 'div';
 
-        return { tag };
+        const comp_variant = computed(() => {
+            if (props.variant) {
+                return props.variant;
+            }
+
+            if (props.blue) {
+                return 'blue';
+            }
+            if (props.gray) {
+                return 'gray';
+            }
+            if (props.green) {
+                return 'green';
+            }
+            if (props.yellow) {
+                return 'yellow';
+            }
+            if (props.red) {
+                return 'red';
+            }
+
+            return 'blue';
+        });
+
+        return { tag, comp_variant };
     },
 });
 </script>
