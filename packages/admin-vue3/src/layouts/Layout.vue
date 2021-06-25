a6e98815e241253d3b462c90793387845d16f2c2
cbl <lennart.carbe@gmail.com>
2021-06-25 14:05:16 +0200

    formatting

--- packages/admin-vue3/src/layouts/Layout.vue
+++ packages/admin-vue3/src/layouts/Layout.vue
@@ -1,13 +1,27 @@
 <template>
     <div class="flex w-screen">
-        <sidebar>
-            <button @click="toggleSidebarSecondary()">
-                Toggle Sidebar Left
-            </button>
-        </sidebar>
-        <sidebar-secondary v-if="show_sidebar_secondary">
+        <aside
+            class="
+                z-10
+                w-64
+                h-screen
+                p-4
+                overflow-y-scroll
+                text-white
+                bg-blue-900
+            "
+        >
+            <header>Admin</header>
+            <nav>
+                <slot name="sidebar" :sidebar="sidebar" />
+            </nav>
+        </aside>
+        <aside
+            class="w-64 h-screen p-4 bg-white border-r border-gray-300 shadow"
+            v-if="sidebar.isOpen"
+        >
             <slot name="sidebar-secondary" />
-        </sidebar-secondary>
+        </aside>
         <div class="flex-1 h-screen overflow-y-scroll">
             <header class="sticky top-0 z-20 w-full bg-white">
                 <nav
@@ -20,12 +34,7 @@
                         border-b border-gray-300
                     "
                 >
-                    <div>
-                        <slot name="header-left" />
-                    </div>
-                    <div>
-                        <slot name="header-right" />
-                    </div>
+                    <slot name="header" :sidebar="sidebar" />
                 </nav>
                 <nav
                     class="
@@ -39,38 +48,45 @@
                         text-gray
                     "
                 >
-                    <div>
-                        <slot name="header-secondary-left" />
-                    </div>
-                    <div>
-                        <slot name="header-secondary-right" />
-                    </div>
+                    <slot name="header-secondary" :sidebar="sidebar" />
                 </nav>
             </header>
             <main class="relative">
-                <slot />
+                <slot :sidebar="sidebar" />
             </main>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import Sidebar from '../components/Sidebar.vue';
-import SidebarSecondary from '../components/SidebarSecondary.vue';
+import { defineComponent, reactive } from 'vue';
 import TransitionSlide from '../transitions/TransitionSlide.vue';
+
+type TSidebar = {
+    isOpen: boolean;
+    show: () => void;
+    close: () => void;
+    toggle: () => void;
+};
+
 export default defineComponent({
-    components: { Sidebar, SidebarSecondary, TransitionSlide },
+    components: { TransitionSlide },
     name: 'Layout',
     setup() {
-        const show_sidebar_secondary = ref<boolean>(false);
-
-        const toggleSidebarSecondary = (): void => {
-            show_sidebar_secondary.value = !show_sidebar_secondary.value;
-        };
+        const sidebar = reactive<TSidebar>({
+            isOpen: false,
+            show() {
+                this.isOpen = true;
+            },
+            close() {
+                this.isOpen = false;
+            },
+            toggle() {
+                this.isOpen = !this.isOpen;
+            },
+        });
         return {
-            show_sidebar_secondary,
-            toggleSidebarSecondary,
+            sidebar,
         };
     },
 });
--- packages/admin-vue3/src/pages/BasePage.vue
+++ packages/admin-vue3/src/pages/BasePage.vue
@@ -1,17 +1,33 @@
 <template>
     <layout>
-        <template v-slot:header-left> foo </template>
-        <template v-slot:header-right> bar </template>
-        <template v-slot:header-secondary-left> baz </template>
-        <template v-slot:header-secondary-right> boom </template>
-        <template v-slot:sidebar-secondary> Nested crud </template>
-        <template v-slot:sidebar-right> Filter </template>
-        <component
-            v-for="(component, index) in components"
-            v-bind="component.props"
-            :is="component.name"
-            :key="index"
-        />
+        <template v-slot:sidebar="{ sidebar }">
+            <ul>
+                <li>
+                    <a href="">Link 1</a>
+                </li>
+                <li>
+                    <a href="">Link 1</a>
+                </li>
+            </ul>
+            <button @click="sidebar.toggle()">Toggle Secondary Sidebar</button>
+        </template>
+        <template v-slot:header>
+            <div>foo</div>
+            <div>bar</div>
+        </template>
+        <template v-slot:header-secondary>
+            <div>baz</div>
+            <div>boom</div>
+        </template>
+        <template v-slot:sidebar-secondary>Nested crud</template>
+        <template v-slot:default>
+            <component
+                v-for="(component, index) in components"
+                v-bind="component.props"
+                :is="component.name"
+                :key="index"
+            />
+        </template>
     </layout>
 </template>
 
