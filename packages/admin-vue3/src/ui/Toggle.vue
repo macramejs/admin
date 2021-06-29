b3ad82775227896e7eb0d722193d6389802d7ab4
Chrissle28 <christian.z.dev@gmail.com>
2021-06-29 17:55:50 +0200

    Added better spacing

--- packages/admin-vue3/src/ui/Toggle.vue
+++ packages/admin-vue3/src/ui/Toggle.vue
@@ -9,14 +9,14 @@
             <Switch
                 v-model="enabled"
                 :class="{
-                    'bg-blue-500 border-blue-500': enabled,
-                    'bg-white border-gray-800': !enabled,
+                    'switch-bg-enabled': enabled,
+                    'switch-bg-disabled': !enabled,
 
                     'text-lg': size_ == 'lg',
                     'text-base': size_ == 'md',
                     'text-xs': size_ == 'sm',
 
-                    'w-14': size_ == 'lg',
+                    'w-16': size_ == 'lg',
                     'w-11': size_ == 'md',
                     'w-9': size_ == 'sm',
 
@@ -24,34 +24,28 @@
                     'h-6  rounded-md': size_ == 'md',
                     'h-4  rounded-sm': size_ == 'sm',
                 }"
-                class="
-                    relative
-                    inline-flex
-                    items-center
-                    transition-all
-                    border
-                    rounded-full
-                    focus:outline-none focus:ring-4 focus:ring-blue-300
-                "
+                class="relative inline-flex items-center transition-all border rounded-full  focus:outline-none focus:ring-4 focus:ring-blue-300"
             >
                 <span
                     :class="{
-                        'translate-x-6 bg-white': enabled,
-                        'translate-x-1 bg-gray-800': !enabled,
+                        'switch-dot-enabled': enabled,
+                        'switch-dot-disabled': !enabled,
 
                         'w-6 h-6': size_ == 'lg',
                         'w-4 h-4': size_ == 'md',
                         'w-2 h-2': size_ == 'sm',
+
+                        'translate-x-2': size_ == 'lg' && !enabled,
+                        'translate-x-1':
+                            (size_ == 'md' && !enabled) ||
+                            (size_ == 'sm' && !enabled),
+
+                        'translate-x-8': size_ == 'lg' && enabled,
+                        'translate-x-6':
+                            (size_ == 'md' && enabled) ||
+                            (size_ == 'sm' && enabled),
                     }"
-                    class="
-                        inline-block
-                        w-4
-                        h-4
-                        transition-all
-                        transform
-                        bg-white
-                        rounded-full
-                    "
+                    class="inline-block transition-all transform bg-white rounded-full "
                 />
             </Switch>
         </div>
@@ -85,3 +79,20 @@ export default {
     },
 };
 </script>
+
+<style>
+.switch-bg-enabled {
+    background-color: #4951f2;
+    border-color: #4951f2;
+}
+.switch-bg-disabled {
+    background-color: #fff;
+    border-color: #5a6776;
+}
+.switch-dot-enabled {
+    background-color: #fff;
+}
+.switch-dot-disabled {
+    background-color: #5a6776;
+}
+</style>
