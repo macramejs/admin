567c81e4cb2fb2c05cb4e4c5cb2356d0eee30875
Jannes Behrens <jannescb@me.com>
2021-06-29 16:26:38 +0200

    Add layout (#21)
    * wip
    
    * sidebar right
    
    * formatting
    
    * wip
    
    Co-authored-by: cbl <lennart.carbe@gmail.com>

---
+++ packages/admin-vue3/src/transitions/TransitionSlide.vue
@@ -0,0 +1,53 @@
+<template>
+    <transition :name="`slide-${to}`">
+        <slot />
+    </transition>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'TransitionSlide',
+    props: {
+        to: {
+            type: String,
+            default: 'left',
+        },
+    },
+    setup() {
+        //
+    },
+});
+</script>
+<style>
+.slide-up-enter-active,
+.slide-down-enter-active,
+.slide-left-enter-active,
+.slide-right-enter-active {
+    transition: all 0.1s ease;
+}
+
+.slide-up-leave-active,
+.slide-down-leave-active,
+.slide-left-leave-active,
+.slide-right-leave-active {
+    transition: all 0.1s ease;
+}
+
+.slide-up-enter-from,
+.slide-up-leave-to {
+    transform: translateY(100%);
+}
+.slide-down-enter-from,
+.slide-down-leave-to {
+    transform: translateY(-100%);
+}
+.slide-left-enter-from,
+.slide-left-leave-to {
+    transform: translateX(100%);
+}
+.slide-right-enter-from,
+.slide-right-leave-to {
+    transform: translateX(-100%);
+}
+</style>
---
+++ packages/admin-vue3/src/ui/Sidebar.vue
@@ -0,0 +1,99 @@
+<template>
+    <TransitionRoot appear :show="open" as="template">
+        <Dialog as="div" v-bind="$attrs" :open="open">
+            <div
+                class="
+                    fixed
+                    top-0
+                    right-0
+                    z-10
+                    w-64
+                    h-full
+                    pt-12
+                    mt-6
+                    bg-white
+                    border-l border-gray-300
+                    shadow
+                "
+            >
+                <aside class="min-h-screen text-center">
+                    <TransitionChild
+                        as="template"
+                        enter="duration-300 ease-out"
+                        enter-from="opacity-0"
+                        enter-to="opacity-100"
+                        leave="duration-200 ease-in"
+                        leave-from="opacity-100"
+                        leave-to="opacity-0"
+                    >
+                        <DialogOverlay class="fixed inset-0" />
+                    </TransitionChild>
+
+                    <TransitionChild
+                        as="template"
+                        enter="duration-300 ease-out"
+                        enter-from="opacity-0 scale-95"
+                        enter-to="opacity-100 scale-100"
+                        leave="duration-200 ease-in"
+                        leave-from="opacity-100 scale-100"
+                        leave-to="opacity-0 scale-95"
+                    >
+                        <div
+                            class="
+                                inline-block
+                                w-full
+                                max-w-md
+                                overflow-y-scroll
+                                text-left
+                                transition-all
+                                transform
+                            "
+                        >
+                            <button @click="$emit('close', false)">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    width="24"
+                                    height="24"
+                                >
+                                    <path fill="none" d="M0 0h24v24H0z" />
+                                    <path
+                                        d="M7.828 11H20v2H7.828l5.364 5.364-1.414 1.414L4 12l7.778-7.778 1.414 1.414z"
+                                    />
+                                </svg>
+                            </button>
+                            <slot />
+                        </div>
+                    </TransitionChild>
+                </aside>
+            </div>
+        </Dialog>
+    </TransitionRoot>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import {
+    TransitionRoot,
+    TransitionChild,
+    Dialog,
+    DialogOverlay,
+    DialogTitle,
+} from '@headlessui/vue';
+
+export default defineComponent({
+    components: {
+        TransitionRoot,
+        TransitionChild,
+        Dialog,
+        DialogOverlay,
+        DialogTitle,
+    },
+    props: {
+        open: {
+            type: Boolean,
+            required: true,
+        },
+    },
+});
+</script>
