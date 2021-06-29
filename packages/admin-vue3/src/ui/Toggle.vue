52ed0558e41aef3f4a2183bd918b72000273aa22
cbl <lennart.carbe@gmail.com>
2021-06-29 16:41:29 +0200

    wip

--- packages/admin-vue3/src/ui/Toggle.vue
+++ packages/admin-vue3/src/ui/Toggle.vue
@@ -1,37 +1,57 @@
 <template>
     <SwitchGroup>
-        <div class="flex flex-col">
-            <SwitchLabel class="mr-4">{{ label }}sss</SwitchLabel>
+        <div class="inline-flex flex-col">
+            <SwitchLabel class="mr-4">
+                <slot>
+                    {{ label }}
+                </slot>
+            </SwitchLabel>
             <Switch
                 v-model="enabled"
                 :class="{
                     'bg-blue-500 border-blue-500': enabled,
                     'bg-white border-gray-800': !enabled,
 
-                    'text-lg': getSize == 'lg',
-                    'text-base': getSize == 'md',
-                    'text-xs': getSize == 'sm',
+                    'text-lg': size_ == 'lg',
+                    'text-base': size_ == 'md',
+                    'text-xs': size_ == 'sm',
 
-                    'w-14': getSize == 'lg',
-                    'w-11': getSize == 'md',
-                    'w-9': getSize == 'sm',
+                    'w-14': size_ == 'lg',
+                    'w-11': size_ == 'md',
+                    'w-9': size_ == 'sm',
 
-                    'h-10  rounded-lg': getSize == 'lg',
-                    'h-6  rounded-md': getSize == 'md',
-                    'h-4  rounded-sm': getSize == 'sm',
+                    'h-10  rounded-lg': size_ == 'lg',
+                    'h-6  rounded-md': size_ == 'md',
+                    'h-4  rounded-sm': size_ == 'sm',
                 }"
-                class="relative inline-flex items-center transition-all border rounded-full  focus:outline-none focus:ring-4 focus:ring-blue-300"
+                class="
+                    relative
+                    inline-flex
+                    items-center
+                    transition-all
+                    border
+                    rounded-full
+                    focus:outline-none focus:ring-4 focus:ring-blue-300
+                "
             >
                 <span
                     :class="{
                         'translate-x-6 bg-white': enabled,
                         'translate-x-1 bg-gray-800': !enabled,
 
-                        'w-6 h-6': getSize == 'lg',
-                        'w-4 h-4': getSize == 'md',
-                        'w-2 h-2': getSize == 'sm',
+                        'w-6 h-6': size_ == 'lg',
+                        'w-4 h-4': size_ == 'md',
+                        'w-2 h-2': size_ == 'sm',
                     }"
-                    class="inline-block w-4 h-4 transition-all transform bg-white rounded-full "
+                    class="
+                        inline-block
+                        w-4
+                        h-4
+                        transition-all
+                        transform
+                        bg-white
+                        rounded-full
+                    "
                 />
             </Switch>
         </div>
@@ -57,10 +77,11 @@ export default {
         ...sizes,
     },
 
-    setup() {
+    setup(props) {
         const enabled = ref(false);
+        const size_ = getSize(props, {});
 
-        return { enabled };
+        return { enabled, size_ };
     },
 };
 </script>
