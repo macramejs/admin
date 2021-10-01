7a393e5ca370d1d27422ef1565217e48f07e36bb
cbl <lennart.carbe@gmail.com>
2021-10-01 12:38:46 +0200

    wip

--- packages/admin-vue3/src/ui/FormSelect.vue
+++ packages/admin-vue3/src/ui/FormSelect.vue
@@ -1,15 +1,13 @@
 <template>
     <div>
-        <field-title v-bind="$attrs" />
-        <form-select v-bind="$attrs" />
+        <select v-bind="$attrs" />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { FormSelect, FieldTitle } from '@macramejs/macrame-vue3';
 
 export default defineComponent({
-    components: { FormSelect, FieldTitle },
+    components: {},
 });
 </script>
