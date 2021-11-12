d3d869d76ec914dc01352ae2987eb080baf9b8f4
cbl <lennart.carbe@gmail.com>
2021-11-12 15:18:25 +0100

    form errors

--- packages/admin-vue3/src/ui/FormField.vue
+++ packages/admin-vue3/src/ui/FormField.vue
@@ -4,16 +4,38 @@
             <FormFieldLabel :label="label" />
         </slot>
         <slot />
+
+        <div v-if="errors">
+            <span v-for="(error, key) in errorList" :key="key" class="text-red">
+                {{ error }}
+            </span>
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from 'vue';
+import { defineProps, computed } from 'vue';
 import FormFieldLabel from './FormFieldLabel.vue';
 
 const props = defineProps({
     label: {
         type: String,
     },
+    errors: {
+        type: [Array, String],
+        default: null,
+    },
+});
+
+const errorList = computed(() => {
+    if (!props.errors) {
+        return [];
+    }
+
+    if (Array.isArray(props.errors)) {
+        return props.errors;
+    }
+
+    return [props.errors];
 });
 </script>
