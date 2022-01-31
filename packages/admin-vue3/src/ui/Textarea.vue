8315a4999c20c2c95b6ff7d4fc83f81502c20790
Christian Zellier <69738385+Chrissle28@users.noreply.github.com>
2022-01-31 14:48:19 +0100

    update textarea (#47)

--- packages/admin-vue3/src/ui/Textarea.vue
+++ packages/admin-vue3/src/ui/Textarea.vue
@@ -1,9 +1,32 @@
 <template>
-    <textarea v-bind="$attrs">
-        <slot />
-    </textarea>
+    <textarea
+        v-bind="$attrs"
+        class="bg-white border w-full focus:outline-none focus:ring-4 focus:ring-orange-100 h-[150px] overflow-y-auto rounded-[8px] p-5"
+        :class="{
+            'border-red-signal': errors.length > 0,
+            'border-gray-500': errors.length == 0,
+        }"
+        :value="modelValue"
+        @input="updateValue"
+    />
 </template>
 
 <script lang="ts" setup>
-//
+const emits = defineEmits(['update:modelValue']);
+
+const props = defineProps({
+    modelValue: String,
+    errors: {
+        type: Array,
+        default: [],
+    },
+    hint: {
+        type: String,
+        default: null,
+    },
+});
+
+const updateValue = (event) => {
+    emits('update:modelValue', event.target.value);
+};
 </script>
