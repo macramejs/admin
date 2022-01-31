b3a33102e9647951ff314ea00fa97df26acd427c
Christian Zellier <69738385+Chrissle28@users.noreply.github.com>
2022-01-31 14:15:24 +0100

    add tab to ui components (#40)
    * add tab to ui components
    
    * housekeeping
    
    Co-authored-by: cbl <lennart.carbe@gmail.com>

---
+++ packages/admin-vue3/src/ui/TabGroup.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="w-full">
+        <TabGroup>
+            <TabList
+                class="flex flex-wrap sticky z-[5] bg-gray-50 top-[80px] w-full px-10 border-b border-gray-200"
+            >
+                <slot name="tab-list" />
+            </TabList>
+            <TabPanels>
+                <slot />
+            </TabPanels>
+        </TabGroup>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { TabGroup, TabList, TabPanels } from '@headlessui/vue';
+</script>
