90e64aad047145948406d1cdbc293967ccabc688
Chrissle28 <christian.z.dev@gmail.com>
2022-01-31 13:56:12 +0100

    add tab to ui components

---
+++ packages/admin-vue3/src/ui/BaseTab.vue
@@ -0,0 +1,32 @@
+<template>
+    <Tab
+        :disabled="disabled"
+        v-slot="{ selected, active }"
+        as="template"
+        class="focus:outline-none"
+        ><button
+            class="px-3 py-1.5 focus:outline-none transition-colors duration-200 text-base font-medium cursor-pointer"
+            :class="{
+                'text-black bg-gray-200 border-b border-indigo-900':
+                    selected && !disabled,
+                'bg-gray-50 border-b border-transparent focus:bg-gray-200 hover:bg-gray-200':
+                    !selected && !disabled,
+                'bg-gray-50 border-b border-transparent text-gray-300 cursor-not-allowed':
+                    disabled,
+            }"
+        >
+            <slot />
+        </button>
+    </Tab>
+</template>
+
+<script setup lang="ts">
+import { Tab } from '@headlessui/vue';
+
+defineProps({
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
---
+++ packages/admin-vue3/src/ui/BaseTabPanel.vue
@@ -0,0 +1,30 @@
+<template>
+    <TabPanel class="relative flex justify-between w-full">
+        <div class="flex-1">
+            <slot />
+        </div>
+        <template v-if="hasSidebar">
+            <div
+                class="w-[233px] sticky bg-white shadow"
+                :style="`height: calc(100vh - ${sidebarTopPosition}px); top: ${sidebarTopPosition}px;`"
+            >
+                <slot name="sidebar" />
+            </div>
+        </template>
+    </TabPanel>
+</template>
+
+<script lang="ts" setup>
+import { TabPanel } from '@headlessui/vue';
+
+defineProps({
+    hasSidebar: {
+        type: Boolean,
+        default: false,
+    },
+    sidebarTopPosition: {
+        type: String,
+        default: '118',
+    },
+});
+</script>
