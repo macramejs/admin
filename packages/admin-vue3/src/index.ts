fabb8b7812d2336da81bf50f1401c518b230f597
Lukas Heller <36259611+lpheller@users.noreply.github.com>
2022-05-17 14:37:14 +0200

    add icons

--- packages/admin-vue3/src/index.ts
+++ packages/admin-vue3/src/index.ts
@@ -97,6 +97,10 @@ const Pages = {
 
 export { Pages, AuthPages };
 
+// Icons
+import IconDraggable from './icons/IconDraggable.vue';
+import IconTrash from './icons/IconTrash.vue';
+export { IconDraggable, IconTrash };
 // Components
 export {
     Card,
