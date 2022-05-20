4e8e116101df39ce6b40b147a0206c46a245d0ae
Lukas Heller <dev@lukas-heller.de>
2022-05-20 11:00:22 +0200

    Fix TreeItem styling (#93)
    * add background and outline styling options
    
    * replace svgs with icon components

--- packages/admin-vue3/src/ui/TreeItem.vue
+++ packages/admin-vue3/src/ui/TreeItem.vue
@@ -2,53 +2,46 @@
     <Disclosure v-slot="{ open }" defaultOpen>
         <div class="flex items-center justify-between w-full group">
             <div
-                class="flex justify-between flex-1 pl-3 pr-2 rounded hover:bg-gray-500 hover:bg-opacity-20 bg-opacity-10"
+                class="flex justify-between flex-1 pl-3 pr-2 rounded"
                 :class="{
-                    'bg-gray-500': isActive,
+                    'bg-gray-500': isActive && background,
+                    'border-gray border': outline,
+                    'hover:bg-gray-500 hover:bg-opacity-20 bg-opacity-10':
+                        background,
                 }"
             >
                 <div
-                    class="flex items-center pr-3 text-gray-100 cursor-move handle"
+                    class="flex items-center pr-3 cursor-move handle"
+                    :class="{
+                        'text-gray-100': background,
+                    }"
                 >
-                    <svg
-                        class="w-2.5 h-2.5 fill-gray"
-                        viewBox="0 0 18 9"
-                        xmlns="http://www.w3.org/2000/svg"
-                        xml:space="preserve"
-                    >
-                        <path
-                            d="M18 7.597a1 1 0 0 0-1-1H1a1 1 0 0 0 0 2h16a1 1 0 0 0 1-1ZM18 1a1 1 0 0 0-1-1H1a1 1 0 0 0 0 2h16a1 1 0 0 0 1-1Z"
-                        />
-                    </svg>
+                    <IconDraggable
+                        class="w-2.5 h-2.5"
+                        :class="{
+                            'fill-gray': background,
+                        }"
+                    />
                 </div>
                 <div
-                    class="flex items-center justify-between flex-1 text-gray-100"
+                    class="flex items-center justify-between flex-1"
+                    :class="{
+                        'text-gray-100': background,
+                    }"
                 >
                     <slot />
                 </div>
             </div>
             <div class="flex items-center w-8">
                 <DisclosureButton
-                    class="p-1 text-gray-100 hover:bg-black rounded-xs"
-                    :class="{ 'rotate-180': !open }"
+                    class="p-1 hover:bg-black rounded-xs"
+                    :class="{
+                        'text-gray-100': background,
+                        'rotate-180': !open,
+                    }"
                     v-if="children?.items.length > 0"
                 >
-                    <svg
-                        width="24"
-                        height="24"
-                        stroke-width="1.5"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="w-3 h-3"
-                    >
-                        <path
-                            d="M6 9L12 15L18 9"
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                        />
-                    </svg>
+                    <IconCaret />
                 </DisclosureButton>
             </div>
         </div>
@@ -60,6 +53,7 @@
 
 <script lang="ts" setup>
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue';
+import { IconDraggable, IconCaret } from '../icons/';
 
 const props = defineProps({
     item: {
@@ -69,6 +63,14 @@ const props = defineProps({
         required: true,
     },
     isActive: {
+        type: Boolean,
+        default: false,
+    },
+    background: {
+        type: Boolean,
+        default: false,
+    },
+    outline: {
         type: Boolean,
         default: false,
     },
