9e729dba310fe08270b75da66f1fdd82ba8c3350
Jannes Behrens <jannes@MacBook-Pro-von-Jannes.fritz.box>
2022-05-25 08:52:37 +0200

    Refactoring

--- packages/admin-vue3/src/ui/SidebarPrimary/SidebarPrimary.vue
+++ packages/admin-vue3/src/ui/SidebarPrimary/SidebarPrimary.vue
@@ -22,7 +22,6 @@
         </nav>
         <slot name="footer">
             <footer class="flex flex-col items-start px-5 py-6 space-y-2">
-                <GlobalSearch />
                 <LockSidebar v-model="locked" />
             </footer>
         </slot>
@@ -33,7 +32,6 @@
 import { ref } from 'vue';
 import { computed } from 'vue';
 import { Link } from '@inertiajs/inertia-vue3';
-import GlobalSearch from './components/GlobalSearch.vue';
 import LockSidebar from './components/LockSidebar.vue';
 
 const props = defineProps({
