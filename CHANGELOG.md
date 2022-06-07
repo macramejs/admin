6ae35dff77c06af38e6f6b71287a0c49a2554d34
Jannes Behrens <jannescb@me.com>
2022-06-07 09:15:49 +0200

    feat/sidebar sec header sidebar ups (#102)
    * release: v0.2.13
    
    * release: v0.2.14
    
    * add sidebar sec header, enhance sidebar link
    
    Co-authored-by: Jannes Behrens <jannes@MacBook-Pro-von-Jannes.fritz.box>
    Co-authored-by: Jannes Behrens <jannes@MBP-von-Jannes.fritz.box>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## [0.2.14](https://github.com/macramejs/admin/compare/v0.2.13...v0.2.14) (2022-06-06)
+
+
+
+## [0.2.13](https://github.com/macramejs/admin/compare/v0.2.12...v0.2.13) (2022-06-06)
+
+
+
 ## [0.2.12](https://github.com/macramejs/admin/compare/v0.2.11...v0.2.12) (2022-06-06)
 
 
---
+++ packages/admin-vue3/src/ui/SidebarSecondaryHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <Header>
+        <span class="inline-block text-xl font-medium">
+            {{ title }}
+        </span>
+        <slot />
+    </Header>
+</template>
+
+<script lang="ts" setup>
+import Header from './Header.vue';
+
+defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+});
+</script>
